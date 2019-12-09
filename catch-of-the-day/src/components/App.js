cb5c2d9d1d5b50f15e67a82c53b75098d87b33c2
Kieran Phillips <keztricks@gmail.com>
2019-12-09 22:17:51 +0000

    Add Inventory & Order module framework

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends React.Component {
     render() {
@@ -7,8 +9,8 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header />
-                    {/* <Inventory />
-                    <Order /> */}
+                    <Inventory />
+                    <Order />
                 </div>
             </div>
         );
