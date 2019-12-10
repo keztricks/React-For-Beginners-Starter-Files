bb7a7fae343a828fb346a0160834aef0894ef1b3
Kieran Phillips <keztricks@gmail.com>
2019-12-10 13:03:10 +0000

    v11 - Create goToStore method bounch to go to store

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,21 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+    MyInput = React.createRef();
+
+    goToStore = event => {
+        event.preventDefault();
+
+        console.log(this);
+    };
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
                 <input
                     type="text"
+                    ref={this.myInput}
                     required
                     placeholder="Store Name"
                     defaultValue={getFunName()}
